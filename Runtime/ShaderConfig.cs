6125b91b90721bd0d274d72edb72235266922403
LouisViel <viel.louis.pro@gmail.com>
2025-04-20 22:13:09 +0200

    17.0.3-LowLight

--- Runtime/ShaderConfig.cs
+++ Runtime/ShaderConfig.cs
@@ -14,12 +14,12 @@ namespace UnityEngine.Rendering.Universal
     public static class ShaderOptions
     {
         /// <summary>Max number of lights supported on mobile with OpenGL 3.0 and below.</summary>
-        public const int k_MaxVisibleLightCountLowEndMobile = 16; // 16
+        public const int k_MaxVisibleLightCountLowEndMobile = 16; // Default - 16. Wanted - 2*6 => 12 (2 point lights)
 
         /// <summary>Max number of lights supported on mobile, OpenGL, and WebGPU platforms.</summary>
-        public const int k_MaxVisibleLightCountMobile = 32; // 32
+        public const int k_MaxVisibleLightCountMobile = 32; // Default - 32. Wanted - 4*6 => 24 (4 point lights)
 
         /// <summary>Max number of lights supported on desktop platforms.</summary>
-        public const int k_MaxVisibleLightCountDesktop = 256; // 256
+        public const int k_MaxVisibleLightCountDesktop = 64; // Default - 256. Wanted - 8*6 => 48 (8 point lights)
     };
 }
